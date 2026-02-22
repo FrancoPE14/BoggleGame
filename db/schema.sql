231eea16febd24811d3611a2d3a38af37109d6e2
jdong79 <jdong79@wisc.edu>
2026-02-22 13:23:15 -0600

    Added User table schema. Closes #71

---
+++ db/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE User (
+    user_name VARCHAR(50) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    matches_won INT DEFAULT 0,
+    highest_score INT DEFAULT 0,
+    PRIMARY KEY (user_name)
+);
