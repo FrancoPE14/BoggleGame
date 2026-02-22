99bf70b36d2952e665e78afd6e6fde55112deadd
jjli8 <jjli8@wisc.edu>
2026-02-22 13:37:10 -0600

    bug fixed for the test controller file

--- backend/src/main/java/com/anteaters/boggle/controller/HomeController.java
+++ backend/src/main/java/com/anteaters/boggle/controller/HomeController.java
@@ -1,7 +1,8 @@
 package com.anteaters.boggle.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
 
 @Controller
 public class HomeController {
