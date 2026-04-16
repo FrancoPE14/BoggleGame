7536dc3bd954fa49bdd683294134faaccf6607fd
jdong79 <jdong79@wisc.edu>
2026-04-15 20:48:34 -0500

    Added setting page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
 
       <div>
         <Button
-          href="/game"
+          href="/setting"
           variant="outline-dark"
           size="lg"
           className="mt-1 mb-1 ms-1 me-1"
