b5a71d926fc108306ece9b1168b764d71bb17b27
pwu84 <bruno7405@gmail.com>
2026-04-19 03:22:40 -0500

    Added lobby page. Closes #200

--- frontend/app/lobbies/components/lobby-card.tsx
+++ frontend/app/lobbies/components/lobby-card.tsx
@@ -1,18 +1,27 @@
 interface LobbyCardProps {
   sessionId: number;
   lobbyname: string;
-  numPlayers: number;
+  playerCount: number;
+  maxPlayers: number;
+  onClick: () => void;
 }
 
 export default function LobbyCard({
   sessionId,
   lobbyname,
-  numPlayers,
+  playerCount,
+  maxPlayers,
+  onClick,
 }: LobbyCardProps) {
   return (
-    <div className="w-full bg-gradient-to-r from-amber-300 to-amber-200 rounded-lg px-6 py-4 flex items-center justify-between shadow-md hover:shadow-lg transition-shadow cursor-pointer">
-      <h3 className="text-white text-lg font-semibold">{lobbyname}</h3>
-      <div className="text-white text-xl font-bold">{numPlayers} / 4</div>
+    <div
+      className="w-full bg-gradient-to-r from-amber-300 to-amber-200 rounded-lg px-4 py-2 my-1 flex items-center justify-between shadow-md hover:shadow-lg transition-shadow cursor-pointer"
+      onClick={onClick}
+    >
+      <h3 className="text-white text-sm font-semibold">{lobbyname}</h3>
+      <div className="text-white text-sm font-bold">
+        {playerCount} / {maxPlayers}
+      </div>
     </div>
   );
 }
--- frontend/app/lobbies/page.tsx
+++ frontend/app/lobbies/page.tsx
@@ -1,8 +1,19 @@
-import LobbyCard from "./components/lobby-card";
+"use client";
 
-import { useEffect } from "react";
+import LobbyCard from "./components/lobby-card";
+import { useEffect, useState } from "react";
+
+interface Lobby {
+  sessionId: number;
+  started: boolean;
+  playerCount: number;
+  maxPlayers: number;
+  hostUsername: string;
+}
 
 export default function LobbyPage() {
+  const [lobbies, setLobbies] = useState<Lobby[]>([]);
+
   useEffect(() => {
     fetch("/api/sessions")
       .then((res) => {
@@ -11,20 +22,31 @@ export default function LobbyPage() {
       })
       .then((data) => {
         console.log(data);
+        setLobbies(data);
       })
       .catch(() => {
         console.error("Could not load session. Please try again later.");
       });
   }, []);
 
+  function enterLobby(sessionId: number) {
+    // enter specifc game lobby based on id
+    console.log("Enter: " + sessionId);
+  }
+
   return (
     <div className="flex min-h-screen min-w-screen w-full max-w-3xl mx-auto flex-col items-center justify-start p-5 bg-amber-100 dark:bg-amber-100">
-      <h1>Lobbies</h1>
-      <LobbyCard
-        sessionId={1234}
-        lobbyname={"Lobby 1"}
-        numPlayers={3}
-      ></LobbyCard>
+      <h1 style={{ margin: 20 }}>Lobbies</h1>
+      {lobbies.map((lobby) => (
+        <LobbyCard
+          key={lobby.sessionId}
+          sessionId={lobby.sessionId}
+          lobbyname={`Lobby ${lobby.sessionId}`}
+          playerCount={lobby.playerCount}
+          maxPlayers={lobby.maxPlayers}
+          onClick={() => enterLobby(lobby.sessionId)}
+        ></LobbyCard>
+      ))}
     </div>
   );
 }
